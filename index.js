dd73a0f3ec14fc22d57fa338f2b2059da5107807
DanielSheehan <daniel.sheehan81@yahoo.com>
2021-10-13 10:26:09 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction (something) {
+    something()
+}
+
+function returnsANamedFunction(){
+    return function daniel(){}
+}
+
+function returnsAnAnonymousFunction(){
+    return function(){}
+}
